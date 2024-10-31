dc965e07a473f416eb5402af69a873cf92e5af70
tomohiro tanaka <tomohiro.tanaka27@gmail.com>
2024-10-31 10:39:45 +0900

    solve no.215

---
+++ code/215.kth-largest-element-in-an-array.go
@@ -0,0 +1,42 @@
+import "container/heap"
+
+/*
+ * @lc app=leetcode id=215 lang=golang
+ *
+ * [215] Kth Largest Element in an Array
+ */
+
+// @lc code=start
+func findKthLargest(nums []int, k int) int {
+	h := &IntHeap{}
+	heap.Init(h)
+
+	for _, num := range nums {
+		heap.Push(h, num)
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+
+	return heap.Pop(h).(int)
+}
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// @lc code=end
