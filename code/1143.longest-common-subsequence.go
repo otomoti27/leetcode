44747ea746622e98fc62ffaafbca100b0070b000
tomohiro tanaka <tomohiro.tanaka27@gmail.com>
2024-11-30 12:01:27 +0900

    solve no.1143

---
+++ code/1143.longest-common-subsequence.go
@@ -0,0 +1,37 @@
+package code
+
+/*
+ * @lc app=leetcode id=1143 lang=golang
+ *
+ * [1143] Longest Common Subsequence
+ */
+
+// @lc code=start
+func longestCommonSubsequence(text1 string, text2 string) int {
+	n, m := len(text1), len(text2)
+	dp := make([][]int, n+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]int, m+1)
+	}
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+
+	return dp[n][m]
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+// @lc code=end
